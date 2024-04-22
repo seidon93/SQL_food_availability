a98336110c0ffb8390a81d736da813817a4cf7e9
Ondřej Seidl <seidon@seznam.cz>
2024-04-23 00:13:42 +0200

    Add result table Q1 and calculation of the % difference

--- SQL/Primary_build.sql
+++ SQL/Primary_build.sql
@@ -24,5 +24,20 @@ SELECT
     GROUP BY payroll_year, industry
     ORDER BY industry, reference_year;
 
+-- výpočet % nárustu/ poklesu mezd
+-- # ((aktuální hodnota) - (počáteční hodnota)) / (počáteční hodnota) * 100
 
 
+SELECT
+    payroll_year AS reference_year,
+    industry.name AS industry,
+    round(avg(value)) AS salary,
+    round(((round(avg(value))- lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) /
+           lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1) AS percentage_yr_grow
+    FROM czechia_payroll AS czpay
+    JOIN czechia_payroll_industry_branch AS industry ON czpay.industry_branch_code = industry.code
+    WHERE payroll_year >= 2006 AND payroll_year <= 2018
+    GROUP BY payroll_year, industry
+    ORDER BY industry, reference_year;
+
+-- --------------------------------------------------------------------------------------------------------------
