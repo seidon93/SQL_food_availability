d1bcef3ba355d9603d8b002c55223b206724e7f2
Ondřej Seidl <seidon@seznam.cz>
2024-04-23 19:37:25 +0200

    table of categories of foods that grow

--- SQL/Primary_q3_table.sql
+++ SQL/Primary_q3_table.sql
@@ -3,8 +3,8 @@ SELECT  name,
         min(round(value)) AS price_min,
         max(year(date_from)) AS year_max,
         max(round(value)) AS price_max,
-        round(((max(round(value)) - min(round(value))) / min(round(value))) * 100,1) AS yr_perc_dif
+        round(((max(round(value)) - min(round(value))) / min(round(value))) * 100,1) AS ΔP_food_dif
 FROM czechia_price_category AS category
 JOIN czechia_price AS cp ON category.code = cp.category_code
 GROUP BY name
-ORDER BY yr_perc_dif;
+ORDER BY ΔP_food_dif;
