d7c802727dfc976dcf7a461bb8d4411cc163d16c
Ondřej Seidl <seidon@seznam.cz>
2024-04-23 19:10:18 +0200

    table tracking the YOY differences in % salary and food price increases of more than 10%

---
+++ SQL/Primary_q4_table.sql
@@ -0,0 +1,32 @@
+WITH salary AS (
+    SELECT  payroll_year,
+            industry.name,
+            round(((round(sum(value)/4) - lag(round(sum(value)/4) ) OVER (PARTITION BY name ORDER BY payroll_year)) /
+            lag(round(sum(value)/4) ) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1) AS percentage_grow_salary
+        FROM czechia_payroll AS czpay
+        JOIN czechia_payroll_industry_branch AS industry ON czpay.industry_branch_code = industry.code
+        WHERE payroll_year >= 2006
+        GROUP BY industry.name, payroll_year ),
+
+prices AS (
+    SELECT
+        name,
+        round(avg(value)) AS price,
+        year(date_from) AS year,
+        round((round(avg(value)) - lag(round(avg(value))) OVER (PARTITION BY name ORDER BY year(date_from))) /
+        lag(round(avg(value))) OVER (PARTITION BY name ORDER BY year(date_from)) * 100, 1) AS perc_food_grow
+    FROM czechia_price_category AS category
+    JOIN czechia_price AS cp ON category.code = cp.category_code
+    GROUP BY name, year
+    ORDER BY name, year )
+
+SELECT s.payroll_year,
+       s.name AS industry,
+       p.name AS product,
+       p.price,
+       s.percentage_grow_salary,
+       p.perc_food_grow - s.percentage_grow_salary AS perc_price_increase
+FROM salary s
+JOIN prices p ON s.payroll_year = p.year
+WHERE p.perc_food_grow - s.percentage_grow_salary > 10
+ORDER BY s.payroll_year, s.name, p.name;
