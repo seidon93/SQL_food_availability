f013f2851b23a9f3b5d94d2a53c08dbf6dcbbed6
Ondřej Seidl <seidon@seznam.cz>
2024-04-27 20:23:54 +0200

    format modifications

--- SQL/Primary_q3_table.sql
+++ SQL/Primary_q3_table.sql
@@ -3,8 +3,9 @@ SELECT  name,
         min(round(value)) AS price_min,
         max(year(date_from)) AS year_max,
         max(round(value)) AS price_max,
-        round(((max(round(value)) - min(round(value))) / min(round(value))) * 100,1) AS ΔP_food_dif
+        concat(round(((max(round(value)) - min(round(value))) / min(round(value))) * 100,1),' %') AS ΔP_food_dif
 FROM czechia_price_category AS category
 JOIN czechia_price AS cp ON category.code = cp.category_code
 GROUP BY name
 ORDER BY ΔP_food_dif;
+
