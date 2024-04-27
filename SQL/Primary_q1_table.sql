bfefbeb4409f62ad6fb9da39782f600ad0f42d55
Ondřej Seidl <seidon@seznam.cz>
2024-04-27 19:58:59 +0200

    format modifications

--- SQL/Primary_q1_table.sql
+++ SQL/Primary_q1_table.sql
@@ -1,11 +1,11 @@
 SELECT
-        payroll_year AS reference_year,
         industry.name AS industry,
-        round(avg(value)) AS salary,
-        round(((round(avg(value))- lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) /
-               lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1) AS percentage_yr_grow
+        payroll_year AS reference_year,
+        concat(round(avg(value)),' Kč') AS salary,
+        concat(round(((round(avg(value))- lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) /
+               lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1), ' %') AS ΔP_yr_grow
         FROM czechia_payroll AS czpay
         JOIN czechia_payroll_industry_branch AS industry ON czpay.industry_branch_code = industry.code
-        WHERE payroll_year >= 2006 AND payroll_year <= 2018
+        WHERE payroll_year >= 2006 AND payroll_year <= 2018 AND value IS NOT NULL
         GROUP BY payroll_year, industry
         ORDER BY industry, reference_year;
