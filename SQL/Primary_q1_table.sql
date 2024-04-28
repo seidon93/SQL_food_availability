748a37535c8f52d089210b95947f4bee07d8ddfc
Ondřej Seidl <seidon@seznam.cz>
2024-04-28 17:57:49 +0200

    Modification columns

--- SQL/Primary_q1_table.sql
+++ SQL/Primary_q1_table.sql
@@ -3,8 +3,11 @@ SELECT
         payroll_year AS reference_year,
         round(avg(value)) AS salary,
         'Kč' AS salary_measure,
+
+        coalesce(
         round(((round(avg(value))- lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) /
-               lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1) AS ΔP_yr_grow,
+        lag( round(avg(value))) OVER (PARTITION BY name ORDER BY payroll_year)) * 100, 1), 0) AS ΔP_yr_grow,
+
         '%' AS ΔP_yr_measure_grow
         FROM czechia_payroll AS czpay
         JOIN czechia_payroll_industry_branch AS industry ON czpay.industry_branch_code = industry.code
